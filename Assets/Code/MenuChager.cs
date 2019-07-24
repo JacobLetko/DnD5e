baf1ec9f5317b7a40d92138e33d422351e24f75b
Jacob Letko <s179061@students.aie.edu.au>
2019-07-24 15:24:37 -0700

    getting started. made simple change screen script

---
+++ Assets/Code/MenuChager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuChager : MonoBehaviour
+{
+    public Gameobject[] screens;
+    
+    public void screenChange(Gameobject target)
+    {
+        foreach (Gameobject screen in screens)
+        {
+            if (screen == target)
+                screen.enabled = true;
+            else
+                screen.enabled = false;
+        }
+    }
+}
